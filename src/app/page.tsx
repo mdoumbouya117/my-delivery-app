b5a78097639bc3a4e724f4548b2059fb7860578c
mohamed-doumbouya <mohamed.doumbouya@externe.maif.fr>
2024-08-04 18:04:11 +0200

    move navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import RestaurantList from '@/components/RestaurantList'
 
 const Home = () => (
 	<div className='min-h-screen flex flex-col'>
-		<Navbar />
 		<main className='flex-grow px-4 sm:px-6 md:px-8'>
 			<h1>Restaurants</h1>
 			<RestaurantList />
