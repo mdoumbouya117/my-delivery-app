547ce272874eb707b29148bdbf2adb302db9fae3
mohamed-doumbouya <mohamed.doumbouya@externe.maif.fr>
2024-08-04 05:56:18 +0200

    Specialites filter

---
+++ src/components/SpecialitiesFilter.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react'
+import { useSearchParams, useRouter } from 'next/navigation'
+
+type SpecialityFilterProps = {
+	specialities: string[]
+	selectedSpeciality: string | null
+	onSelect: (speciality: string) => void
+}
+
+const SpecialityFilter = ({
+	specialities,
+	selectedSpeciality,
+	onSelect,
+}: SpecialityFilterProps) => {
+	const router = useRouter()
+	const searchParams = useSearchParams()
+
+	const handleSpecialityClick = (speciality: string) => {
+		onSelect(speciality)
+		const params = new URLSearchParams(searchParams.toString())
+		params.set('speciality', speciality)
+		router.push(`?${params.toString()}`)
+	}
+
+	useEffect(() => {
+		const speciality = searchParams.get('speciality')
+		if (speciality) {
+			onSelect(speciality)
+		}
+	}, [onSelect, searchParams])
+
+	return (
+		<div className='flex overflow-x-auto space-x-4 pr-4 py-4'>
+			{specialities.map((speciality) => (
+				<button
+					key={speciality}
+					className={`flex-shrink-0 text-sm font-semibold py-2 px-4 rounded-full shadow-md cursor-pointer transition-colors duration-300 
+                        ${
+									selectedSpeciality === speciality
+										? 'bg-[#134571] text-white'
+										: 'text-black'
+								}`}
+					onClick={() => handleSpecialityClick(speciality)}
+				>
+					{speciality}
+				</button>
+			))}
+		</div>
+	)
+}
+
+export default SpecialityFilter
