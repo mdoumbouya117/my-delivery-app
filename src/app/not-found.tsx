16fd0c4520588afa42470ae3d8a86fd17b0f8415
mohamed-doumbouya <mohamed.doumbouya@externe.maif.fr>
2024-08-04 08:55:16 +0200

    not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+
+const NotFoundPage = () => (
+	<div className='flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white p-4'>
+		<div className='max-w-md text-center'>
+			<h1 className='text-5xl font-bold mb-4'>Page Not Found</h1>
+			<p className='text-lg mb-6'>
+				Oops! The page you are looking for does not exist. It might have
+				been moved or deleted.
+			</p>
+			<Link
+				href='/'
+				className='bg-white text-blue-500 px-6 py-3 rounded-full shadow-md hover:bg-gray-200 transition-colors duration-300'
+			>
+				Go to Homepage
+			</Link>
+		</div>
+	</div>
+)
+
+export default NotFoundPage
