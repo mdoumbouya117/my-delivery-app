a6d9876802591668e0ccc6b336123f26be0fc8a6
Mohamed Doumbouya <mohamed.doumbouya@externe.maif.fr>
2024-09-03 00:29:39 +0200

    update api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,21 @@
-import { restaurants } from "@/shared/data"
+import { couriers, customers, orders, restaurants } from "@/shared/data";
+
+export const getRestaurants = () => {
+  return restaurants;
+};
 
 export const getRestaurantById = (id: string) => {
-    return restaurants.find(restaurant => restaurant.id === id)
-}
+  return restaurants.find((restaurant) => restaurant.id === id);
+};
+
+export const getCouriers = () => {
+  return couriers;
+};
+
+export const getOrders = () => {
+  return orders;
+};
+
+export const getCustomers = () => {
+  return customers;
+};
